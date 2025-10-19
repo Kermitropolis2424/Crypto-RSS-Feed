50de13897a01d8f564f090fd3646c3d1d29485d3
Kermitropolis2424 <likespun@gmail.com>
2025-10-18 21:14:39 -0400

    four

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10060960
+    "zoneId": 10060971
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
